a5396645ad82a6f5e745386f38bab1d698adfb49
Nick Booker <nick.booker@opusvl.com>
2015-11-11 16:53:57 +0000

    Use Test::PostgreSQL

--- share/profiles/FB11X/skel/t/lib/TestApp.pm
+++ share/profiles/FB11X/skel/t/lib/TestApp.pm
@@ -9,4 +9,13 @@ my $builder = TestApp::Builder->new(
 
 $builder->bootstrap;
 
+TestApp->model('FB11AuthDB')->schema->deploy({add_drop_table => 1});
+TestApp->model('FB11AuthDB::User')->create({
+    username => 'fb11admin',
+    password => 'password',
+    email => 'support@opusvl.com',
+    name => 'Admin',
+    tel => '044444'
+});
+
 1;
--- share/profiles/FB11X/skel/t/lib/TestApp/Builder.pm
+++ share/profiles/FB11X/skel/t/lib/TestApp/Builder.pm
@@ -1,7 +1,12 @@
 package TestApp::Builder;
+use Test::PostgreSQL;
 
 use Moose;
 extends 'OpusVL::FB11::Builder';
+
+# Just keep it in scope until the process ends.
+our $PSQL;
+
 
 override _build_superclasses => sub
 {
@@ -21,14 +26,16 @@ override _build_plugins => sub {
 override _build_config => sub {
     my $self   = shift;
     my $config = super(); # Get what CatalystX::AppBuilder gives you
+    $PSQL = Test::PostgreSQL->new
+        or die $Test::PostgreSQL::errstr;
 
-    # point the FB11Auth Model to the correct DB file....
-    $config->{'Model::FB11AuthDB'} = 
-    {
+
+
+    # point the FB11AuthDB Model to the temporary database
+    $config->{'Model::FB11AuthDB'} = {
         schema_class => 'OpusVL::FB11::Schema::FB11AuthDB',
-        connect_info => [
-          'dbi:SQLite:' . TestApp->path_to('root','fb11-auth.db'),
-        ],
+        connect_info => [ $PSQL->dsn ],
+        on_connect_do => 'SET client_min_messages=WARNING',
     };
 
     # .. add static dir into the config for Static::Simple..
@@ -37,10 +44,10 @@ override _build_config => sub {
     $config->{static}->{include_path} = $static_dirs;
     
     # .. allow access regardless of ACL rules...
-    $config->{'appkit_can_access_actionpaths'} = ['custom/custom'];
+    $config->{'fb11_can_access_actionpaths'} = ['custom/custom'];
 
     # DEBUGIN!!!!
-    $config->{'appkit_can_access_everything'} = 1;
+    $config->{'fb11_can_access_everything'} = 1;
     
     $config->{application_name} = 'FB11 TestApp';
     $config->{default_view}     = 'FB11TT';
