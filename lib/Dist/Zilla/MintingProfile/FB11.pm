1a6605954c5a4a6da8b66c50f8af328819bf2662
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2016-06-21 12:44:22 +0100

    use base ShareDir profile role

--- lib/Dist/Zilla/MintingProfile/FB11.pm
+++ lib/Dist/Zilla/MintingProfile/FB11.pm
@@ -4,26 +4,21 @@ package Dist::Zilla::MintingProfile::FB11;
 
 our $VERSION = '0.008';
 use Moose;
-with 'Dist::Zilla::Role::MintingProfile';
+use 5.014;
+with 'Dist::Zilla::Role::MintingProfile::ShareDir';
 use File::ShareDir;
 use Path::Class;        # sadly, we still need to use Path::Class :(
 use Carp;
 use namespace::autoclean;
 
-sub profile_dir
+around profile_dir => sub
 {
-    my ($self, $profile_name) = @_;
+    my ($orig, $self, $profile_name) = @_;
 
-    die 'minting requires perl 5.014' unless $] >= 5.013002;
+    $profile_name ||= 'default';
 
-    my $dist_name = '{{ $dist->name }}';
-    my $profile_dir = dir( File::ShareDir::dist_dir($dist_name) )
-                      ->subdir( 'profiles', $profile_name );
-
-    return $profile_dir if -d $profile_dir;
-
-    confess "Can't find profile $profile_name via $self: it should be in $profile_dir";
-}
+    say and return $_ for $self->$orig($profile_name);
+};
 
 __PACKAGE__->meta->make_immutable;
 
