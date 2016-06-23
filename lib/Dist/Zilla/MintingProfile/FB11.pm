85e30e9a1cd3302292ec30c3418e0106ccb973d6
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2016-06-23 19:19:55 +0100

    Moved back to share/

--- lib/Dist/Zilla/MintingProfile/FB11.pm
+++ lib/Dist/Zilla/MintingProfile/FB11.pm
@@ -6,19 +6,7 @@ our $VERSION = '0.010';
 use Moose;
 use 5.014;
 with 'Dist::Zilla::Role::MintingProfile::ShareDir';
-use File::ShareDir;
-use Path::Class;        # sadly, we still need to use Path::Class :(
-use Carp;
 use namespace::autoclean;
-
-around profile_dir => sub
-{
-    my ($orig, $self, $profile_name) = @_;
-
-    $profile_name ||= 'default';
-
-    return $self->$orig($profile_name);
-};
 
 __PACKAGE__->meta->make_immutable;
 
