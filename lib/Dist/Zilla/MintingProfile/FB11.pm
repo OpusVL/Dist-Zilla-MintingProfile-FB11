dbdc0120a0c9bddaf3af851418ac43bd82427f7e
Nick Booker <nick.booker@opusvl.com>
2016-06-27 15:48:56 +0100

    Bumped version number

--- lib/Dist/Zilla/MintingProfile/FB11.pm
+++ lib/Dist/Zilla/MintingProfile/FB11.pm
@@ -1,7 +1,7 @@
 package Dist::Zilla::MintingProfile::FB11;
 
 # ABSTRACT: Mints a new FB11 site
-our $VERSION = '0.015';
+our $VERSION = '0.016';
 use Moose;
 use 5.014;
 with 'Dist::Zilla::Role::MintingProfile::ShareDir';
