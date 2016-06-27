22733b219f34b02b24634e8b8653c2e9409995bd
Nick Booker <nick.booker@opusvl.com>
2016-06-27 12:10:26 +0100

    Add POD for minting profile

--- lib/Dist/Zilla/MintingProfile/FB11.pm
+++ lib/Dist/Zilla/MintingProfile/FB11.pm
@@ -11,3 +11,31 @@ use namespace::autoclean;
 __PACKAGE__->meta->make_immutable;
 
 1;
+
+
+=head1 NAME
+
+Dist::Zilla::MintingProfile::FB11 - Mints a new FB11 site
+
+=head1 PROFILES
+
+=head2 default
+
+Mints a new FB11 top-level application.
+
+This is the default if you don't provide C<-p>.
+
+    dzil new -P FB11 MyApp
+
+=head2 FB11X
+
+Mints a new FB11X component.
+
+    dzil new -P FB11 -p FB11X MyApp::FB11X::ComponentName
+
+=head2 Form
+
+Mints a new form (use with C<dzil add>)
+
+    cd MyApp-FB11X-ComponentName
+    dzil add -P FB11 -p Form MyApp::FB11X::ComponentName::Form::EntityName
