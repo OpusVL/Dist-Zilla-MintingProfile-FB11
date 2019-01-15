bbd01686cbfb06086e20b0d52d43b27ec7e5cca6
Alastair Douglas <alastair.douglas@gmail.com>
2019-01-15 13:00:20 +0000

    Bumped version number

--- lib/Dist/Zilla/MintingProfile/FB11.pm
+++ lib/Dist/Zilla/MintingProfile/FB11.pm
@@ -1,7 +1,7 @@
 package Dist::Zilla::MintingProfile::FB11;
 
 # ABSTRACT: Mints a new FB11 site
-our $VERSION = '0.018';
+our $VERSION = '0.019';
 use Moose;
 use 5.014;
 with 'Dist::Zilla::Role::MintingProfile::ShareDir';
